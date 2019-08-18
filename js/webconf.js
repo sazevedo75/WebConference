86919817fa49858b2ce7ebebe8a8ea06f6afe7da
sazevedo75 <sergio.azevedo.learning@gmail.com>
2019-08-18 20:43:55 +0100

    Vária altereções

--- js/webconf.js
+++ js/webconf.js
@@ -1,8 +1,10 @@
 window.onload = function()
 {
+    const urlBase = "https://fcawebbook.herokuapp.com"
+
     const btnRegister = document.getElementById("btnRegister")
 
-    // Botão de Registo
+    // Botão de Registo de Participantes
     btnRegister.addEventListener("click", function()
     {
         swal(
@@ -53,4 +55,83 @@ window.onload = function()
                     }
                 })
     })
+    // END Botão de Registo de Participantes
+
+//--------------------------------------------------------------------------------------------
+
+    // SPEAKERS
+
+    (
+        async() =>
+        {
+            const renderSpeakers = document.getElementById("renderSpeakers")
+            let txtSpeakers = ""
+            const response = await fetch(`${urlBase}/conferences/1/speakers`)
+            const speakers = await response.json()
+
+            for(const speaker of speakers)
+            {
+                txtSpeakers += `
+                                    <div class="col-sm-4">
+                                        <div class="team-member">
+                                            <img class="mx-auto rounded-circle" src="${speaker.foto}" alt="">
+                                            <h4>${speaker.nome}</h4>
+                                            <p class="text-muted">${speaker.cargo}</p>
+                                            <ul class="list-inline social-buttons">
+                                `
+                                            if(speaker.twitter !== null)
+                                            {
+                                                txtSpeakers += `
+                                                                    <li class="list-inline-item">
+                                                                    <a href="${speaker.twitter}" target="_blank">
+                                                                        <i class="fab fa-twitter"></i>
+                                                                    </a>
+                                                                    </li>
+                                                                `
+                                            }
+
+                                            if(speaker.facebook !== null)
+                                            {
+                                                txtSpeakers += `
+                                                                    <li class="list-inline-item">
+                                                                    <a href="${speaker.facebook}" target="_blank">
+                                                                        <i class="fab fa-facebook-f"></i>
+                                                                    </a>
+                                                                    </li>
+                                                                `
+                                            }
+
+                                            if(speaker.linkedin !== null)
+                                            {
+                                                txtSpeakers += `
+                                                                    <li class="list-inline-item">
+                                                                    <a href="${speaker.linkedin}" target="_blank">
+                                                                        <i class="fab fa-linkedin-in"></i>
+                                                                    </a>
+                                                                    </li>
+                                                                `
+                                            }
+
+                                            txtSpeakers += `
+                                                                <li class="list-inline-item">
+                                                                <a href="${speaker.linkedin}" target="_blank">
+                                                                    <i class="fab fa-linkedin-in"></i>
+                                                                </a>
+                                                                </li>
+                                                            `
+                                            txtSpeakers +=`  
+                                            </ul>
+                                        </div>
+                                    </div>
+                                 `
+            }
+            renderSpeakers.innerHTML = txtSpeakers
+        }
+    )()
+
+    // END SPEAKERS
+
+
+
+
 }
